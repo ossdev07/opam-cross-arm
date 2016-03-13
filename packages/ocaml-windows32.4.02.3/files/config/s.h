57e8d5a878872c494c32b4f14e02277f20b09e4c
whitequark <whitequark@whitequark.org>
2016-03-13 07:04:09 +0000

    +conf-gcc-windows{32,64} +flexdll-windows.0.34 +ocaml-windows{,32,64}.4.03.0

---
+++ packages/ocaml-windows32.4.02.3/files/config/s.h
@@ -0,0 +1,15 @@
+#define OCAML_OS_TYPE "Win32"
+
+#undef BSD_SIGNALS
+#define HAS_STRERROR
+#define HAS_SOCKETS
+#define HAS_GETCWD
+#define HAS_UTIME
+#define HAS_DUP2
+#define HAS_GETHOSTNAME
+#define HAS_MKTIME
+#define HAS_PUTENV
+#define HAS_LOCALE
+#define HAS_BROKEN_PRINTF
+#define HAS_IPV6
+#define HAS_NICE
